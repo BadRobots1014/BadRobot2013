0b7d3c33001f5c3a100db542e323c189a852ae22
Paul Chao <paulchao111@gmail.com>
2013-01-29 12:00:40 -0500

    new command to test the triggers.

---
+++ src/com/badrobot/commands/triggerToShoot.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.badrobot.commands;
+
+import com.badrobot.OI;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import com.badrobot.commands.Shoot;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *SAME CODE AS SHOOT WITH CONTROLLER, JUST THAT TRIGGER IS TESTED
+ * @author Jon Buckley
+ */
+public class triggerToShoot extends CommandBase
+{
+    boolean runShooter;
+    double shooterSpeed;
+    
+    public triggerToShoot()
+    {
+        requires((Subsystem) shooter);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize()
+    {
+        shooterSpeed = .4;
+        runShooter = false;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute()
+    {
+        //TESTING THE TRIGGER
+        if (OI.primaryXboxController.getTrigger(GenericHID.Hand.kLeft))
+        {
+            if (!runShooter)
+            {
+                shooter.runShooter(shooterSpeed);
+                runShooter = true;
+            }
+            
+            else
+            {
+                shooter.runShooter(0);
+                runShooter = false;
+            }            
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished()
+    {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end()
+    {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted()
+    {
+    }
+}
