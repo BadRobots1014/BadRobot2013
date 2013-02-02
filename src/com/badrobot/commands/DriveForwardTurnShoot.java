fd1b3cff9781ece26acb8a22ba42b101d4d9ad2f
Kyle Compton <qmnascarfan4@gmail.com>
2013-02-02 10:53:03 -0500

    added DriveForwardTurnShoot command group and Turn command

--- src/com/badrobot/commands/DriveForwardTurnShoot.java
+++ src/com/badrobot/commands/DriveForwardTurnShoot.java
@@ -15,24 +15,8 @@ public class DriveForwardTurnShoot extends CommandGroup
     
     public DriveForwardTurnShoot() 
     {
-        addSequential(new DriveForward());
+        addSequential(new DriveStraightForward(3));
         addSequential(new Turn(90));
         addSequential(new Shoot());
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
     }
 }
